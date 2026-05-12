1ed8f0779f893573e5996516555eec1977e978fc
Rooha <rooha.codes@gmail.com>
2026-05-12 23:04:06 +0500

    vite.config.js

--- student-lms/frontend/vite.config.js
+++ student-lms/frontend/vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   base: "/student-lms-management-system/",
-  plugins: [react()],
-});
-export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+});
