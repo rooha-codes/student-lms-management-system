655168a067f40ecad1dd4e083da690cc69714872
Rooha <rooha.codes@gmail.com>
2026-05-13 22:53:09 +0500

    api.js

--- student-lms/frontend/src/api.js
+++ student-lms/frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://a209feb9-4b07-4b4c-9ea6-2ab9095c7e43-00-s1xpw4sjxv0y.sisko.replit.dev/api",
 });
 
 // ✅ Attach token on every request
