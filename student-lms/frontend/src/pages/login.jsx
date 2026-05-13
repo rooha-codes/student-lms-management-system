ecc93d511ae3ea0939388d3705370149f79d54e8
Rooha <rooha.codes@gmail.com>
2026-05-13 23:20:22 +0500

    login.jsx

--- student-lms/frontend/src/pages/login.jsx
+++ student-lms/frontend/src/pages/login.jsx
@@ -37,7 +37,7 @@ function Login() {
       setError("");
 
       const res = await axios.post(
-        "http://localhost:5001/api/auth/login",
+  "https://a209feb9-4b07-4b4c-9ea6-2ab9095c7e43-00-s1xpw4sjxv0y.sisko.replit.dev/api/auth/login",
         {
           email: form.email.trim(),
           password: form.password,
